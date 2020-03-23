7446aabaac76faf7448ead8bab2a86098107dd31
jv658 <jvandermost658@gmail.com>
2020-03-23 19:01:42 -0400

    Initial Commit

---
+++ photorama/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  photorama
+//
+//  Created by Joshua Vandermost on 2020-03-23.
+//  Copyright © 2020 Joshua Vandermost. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ photoramaTests/photoramaTests.swift
@@ -0,0 +1,34 @@
+//
+//  photoramaTests.swift
+//  photoramaTests
+//
+//  Created by Joshua Vandermost on 2020-03-23.
+//  Copyright © 2020 Joshua Vandermost. All rights reserved.
+//
+
+import XCTest
+@testable import photorama
+
+class photoramaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
